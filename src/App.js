0bf1e3f59950560b0c02f823aa844bfc1f12ebd6
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-15 12:58:25 -0700

    removing array storing notes in index file and moving storage to database accessed using axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("")
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -24,29 +36,29 @@ const App = (props) => {
   }
 
   const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
+  ? notes
+  : notes.filter(note => note.important)
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
+    <h1>Notes</h1>
+    <div>
+    <button onClick={() => setShowAll(!showAll)}>
+    show {showAll ? 'important' : 'all' }
+    </button>
+    </div>
+    <ul>
+    {notesToShow.map(note =>
+      <Note key={note.id} note={note} />
+    )}
+    </ul>
+    <form onSubmit={addNote}>
+    <input
+    value={newNote}
+    onChange={handleNoteChange}
+    />
+    <button type="submit">save</button>
+    </form>
     </div>
   )
 }
