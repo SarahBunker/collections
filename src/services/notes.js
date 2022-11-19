ae57de845190d36140cd94f22bbc903bf88e117c
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-18 18:16:06 -0700

    adding proxy and creating a new build

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,17 +1,21 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+// const baseUrl = 'http://localhost:8080/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
+  console.log("Made it to getAll")
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
 const create = newObject => {
+  console.log("Made it to create")
   const request = axios.post(baseUrl, newObject)
   return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
+  console.log("Made it to update")
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
